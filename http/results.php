913a1398c90d72750fb6b410a2497b6a9b5d7096
bstchan <bstchan@rocketmail.com>
2016-11-06 09:43:59 -0800

    update navbar links

--- http/results.php
+++ http/results.php
@@ -34,15 +34,15 @@
                 <ul class="nav navbar-nav">
                     <li><a href="index.php">
                         <span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home</a></li>
-                    <li><a ui-sref="survey.html">
+                    <li><a ui-sref="survey.php">
                          <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Take a survey</a></li>
-					<li><a href="storyshare.html">
+					<li><a href="storyshare.php">
                         <span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> Tell me what happened</a></li>
                     <li><a ui-sref="#">
 						 <span class="glyphicon glyphicon-stats" aria-hidden="true"></span> Results &amp; Resources</a></li>
-                    <li><a ui-sref="resources.html">
+                    <li><a ui-sref="resources.php">
 						 <span class="glyphicon glyphicon-tree-conifer" aria-hidden="true"></span> Wellness &amp; Self-Care</a></li>
-					<li><a ui-sref="contactus.html">
+					<li><a ui-sref="contactus.php">
 						 <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> Contact Us</a></li>
                 </ul>
                 
