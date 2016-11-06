c3da98d5b5c8f8f00134e1d41033c17cc55c5d4b
Chrisknyfe <chrisknyfe@gmail.com>
2016-11-05 20:08:42 -0700

    First Page

---
+++ http/index.php
@@ -0,0 +1,7 @@
+<head>
+<title>SafeBridge - Home</title>
+</head>
+<body>
+	<h1>SafeBridge</h1>
+	<p>A web app to connect survivors of sexual assault to helpful resources and to help them report their experiences in a safe way. For the Stanford Health++ Hackathon. <a href="http://healthplusplus.stanford.edu/">href=http://healthplusplus.stanford.edu/</a></p>
+</body>
