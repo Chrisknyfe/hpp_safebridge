a456d9a48e9660866754e2d44e3c1b077c41bcc9
bstchan <bstchan@rocketmail.com>
2016-11-06 14:04:05 -0800

    redid columns

--- http/results.php
+++ http/results.php
@@ -55,13 +55,12 @@
 			<h1>What does it mean to be 'healthy'? Where do I stand?</h1>
 		</div>
 		
-		
 		<!-- Slider or bar graph showing comparison to what is healthy based on the responses of other users (Results 1) -->
 		<div class="row row-content">
-			<h4><p>Based on the details you shared, you may be a survivor of sexual violence.  We understand that you have experienced a stressful situation, and want you to know that you have made the right decision to understand your circumstances and take control of your next steps<br>
+			<h4><p>Based on the details you shared, you may be a survivor of sexual violence.  We understand that you have experienced a stressful situation, and want you to know that you have made the right decision to understand your circumstances and take control of your next steps.<br>
 			</p></h4>
 
-			<p> It sounds like this event occured between you and someone you know. You indicated that nonconsensual penetration or nonconsensual oral sex occured by someone taking advantage of you when you were drunk or high.  You did not indicate that the act occured while you were asleep, unconscious, or unable to resist or respond.  You may be unsure of how to deal with your future relationship with this person. </p>
+			<p> It sounds like this event occurred between you and someone you know. You indicated that nonconsensual penetration or nonconsensual oral sex occured by someone taking advantage of you when you were drunk or high.  You did not indicate that the act occured while you were asleep, unconscious, or unable to resist or respond.  You may be unsure of how to deal with your future relationship with this person.</p>
 
 			<p>Sexual assault is any type of sexual contact or behavior that occurs without the explicit consent of the recipient. Falling under the definition of sexual assault are sexual activities as forced sexual intercourse, forcible sodomy, child molestation, incest, fondling, and attempted rape.</p>
 
@@ -69,15 +68,8 @@
 
 			<P> Of those respondents, 15% contacted CST, and 5% went to the ER.  35% considered ending contact with the other person. </P>
 
-			<div class="progress" id="standing">
-				<div class="progress-bar progress-bar-danger" style="width: 10%">
-					<span class="sr-only">10% Complete (danger)</span>
-				</div>
-				<div class="progress-bar progress-bar-warning progress-bar-striped active" style="width: 40%">
-					<span class="sr-only">20% Complete (warning)</span>
-				</div>
-				<div class="progress-bar progress-bar-success" style="width: 15%">
-					<span class="sr-only">35% Complete (success)</span>
+			<div class="progress" id="separator">
+				<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
 				</div>
 			</div>
 		</div>
@@ -86,75 +78,64 @@
 		<!-- Results 2, 3 -->
 		<div class="row row-content">
 			
-			<div class="col-sm-4">
+			<div class="col-xs-6 col-sm-3">
 				<h4>Your current emotional state and behaviors may suggest symptoms of <strong>an anxiety or depression disorder</strong>.</h4>
 				<div class="well well-lg">
-					In addition to the physical effects of sexual violence, we know that some survivors of sexual violence are at high risk of experiencing both short and long-term psychological conditions including PTSD, depression, and phobias, and are at increased risk for revictimization.
+					In addition to the physical effects of sexual violence, we know that some survivors of sexual violence are at high risk of experiencing both short and long-term psychological conditions including PTSD, depression, and phobias, and are at increased risk for revictimization.<br><br>
 					Victims of disaster and other traumatic events often require immediate psychological and medical care. Therefore, routine screening for posttraumatic stress disorder (PTSD) in such individuals is highly recommended.
 				</div>
 			</div>
 			
-			<div class="col-sm-4">
-				<h4><a href = "https://adminguide.stanford.edu/chapter-1/subchapter-7/policy-1-7-3#anchor-24469">Stanford's Prohibited Sexual Conduct Policy</a> classifies your experience as <strong>Sexual Misconduct</strong>.</h4>
+			<div class="col-xs-6 col-sm-3">
+				<h4><a href="https://adminguide.stanford.edu/chapter-1/subchapter-7/policy-1-7-3#anchor-24469">Stanford's Prohibited Sexual Conduct Policy</a> classifies your experience as <strong>Sexual Misconduct</strong>.</h4>
 				<div class="well well-lg">
 					<p>Sexual misconduct is the commission of a sexual act, whether by a stranger or nonstranger and regardless of the gender of any party, which occurs without indication of consent.<br></p>
-
-<p>1. The following acts or attempted acts can be the subject of a Sexual Misconduct or Sexual Assault charge:<br>
-a) vaginal or anal intercourse;<br>
-b) digital penetration;<br>
-c) oral copulation; or<br>
-d) penetration with a foreign object<br></p>
-
-<p>2.  Additional Acts of Sexual Misconduct<br>
-The following completed acts can be the subject of a Sexual Misconduct charge:
-a) unwanted touching or kissing of an intimate body part (whether directly or through clothing); or <br>
-b) recording, photographing, transmitting, viewing or distributing intimate or sexual images without the knowledge and consent of all parties involved.<br></p>
+					<p>1. The following acts or attempted acts can be the subject of a Sexual Misconduct or Sexual Assault charge:<br>
+					a) vaginal or anal intercourse;<br>
+					b) digital penetration;<br>
+					c) oral copulation; or<br>
+					d) penetration with a foreign object.<br></p>
+					
+					<p>2.  Additional Acts of Sexual Misconduct<br>
+					The following completed acts can be the subject of a Sexual Misconduct charge:
+					a) unwanted touching or kissing of an intimate body part (whether directly or through clothing); or <br>
+					b) recording, photographing, transmitting, viewing or distributing intimate or sexual images without the knowledge and consent of all parties involved.<br></p>
 				</div>
 			</div>
 			
-			<div class="col-sm-4">
-				<h4><u>Your recommended next steps</u></h4><br>
+			<div class="col-xs-6 col-sm-3">
+				<h4><u>Your recommended next steps</u></h4>
+				<p><h4>Because the incident occurred recently, we suggest <strong>Emergency Assistance</strong> through the following resources.</h4></p><br>
 				<ul class="list-unstyled">
-				<li>
+					<li>
 						<blockquote>
-							<p>Because the incident occured recently, we suggest<b>Emergency Assistance</b><br></p>
-							
+							<p><a href="http://www.vaden.stanford.edu/sexual-assault">Confidential Support Team (CST)</a><br>Confidential support and help accessing resources, short-term emotional support, and ongoing individual counseling; free to Stanford students.</p>
 							<address>
 							866 Campus Drive<br>
 							Stanford, CA 94305-8580<br>
 							<!-- <span class="glyphicon glyphicon-earphone"></span> (650) 498-2336 -->
-							<a class="btn btn-primary btn-xlg" href="(650) 725-9955"><span class="glyphicon glyphicon-earphone"></span> Click to Call 24/7
+							<a class="btn btn-primary btn-xlg" href="tel:650-725-9955">
+								<span class="glyphicon glyphicon-earphone"></span> Click to Call 24/7
 							</a>
 							</address>
 						</blockquote>
 					</li>
 					<li>
 						<blockquote>
-							<p><a href="http://www.vaden.stanford.edu/sexual-assault">Confidential Support Team (CST)</a>Confidential support and help accessing resources, short-term emotional support, and ongoing individual counseling; free to Stanford students</p>
+							<p><a href="https://www.vaden.stanford.edu/make-appointment/counseling-and-psychological-services">Counseling and Psychological Services</a></p>
 							<address>
-							866 Campus Drive<br>
-							Stanford, CA 94305-8580<br>
-							<!-- <span class="glyphicon glyphicon-earphone"></span> (650) 498-2336 -->
-							<a class="btn btn-primary btn-xlg" href="(650) 725-9955"><span class="glyphicon glyphicon-earphone"></span> Click to Call 24/7
-							</a>
-							</address>
-						</blockquote>
-					</li>
-					<li>
-						<blockquote>
-							<p><a href="https://vaden.stanford.edu/make-appointment/counseling-and-psychological-services">Counseling and Psychological Services</a></p>
-							<address>
-							866 Campus Drive<br>
-							Stanford, CA 94305-8580<br>
-							<!-- <span class="glyphicon glyphicon-earphone"></span> (650) 498-2336 -->
-							<a class="btn btn-primary btn-xlg" href="(650) 498-2336"><span class="glyphicon glyphicon-earphone"></span> Click to Call
-							</a>
+								866 Campus Drive<br>
+								Stanford, CA 94305-8580<br>
+								<!-- <span class="glyphicon glyphicon-earphone"></span> (650) 498-2336 -->
+								<a class="btn btn-primary btn-xlg" href="tel:650-498-2336">
+									<span class="glyphicon glyphicon-earphone"></span> Click to Call
+								</a>
 							</address>
 						</blockquote>
 					</li>
 				</ul>
 			</div>
-			<div class="col-sm-4">
+			<div class="col-xs-6 col-sm-3">
 				<h4><u>Further resources off-campus</u></h4><br>
 				<ul class="list-unstyled">
 					<li>
@@ -162,7 +143,8 @@ b) recording, photographing, transmitting, viewing or distributing intimate or s
 							<p><a href="http://calyouth.org/ca-youth-crisis-line/">California Youth Crisis Line</a></p>
 							24/7 free, confidential service for persons in need up to age 24.<br>
 							<!-- <span class="glyphicon glyphicon-earphone"></span> 1-800-843-5200 -->
-							<a class="btn btn-primary btn-xlg" href="1-800-843-5200"><span class="glyphicon glyphicon-earphone"></span> Click to Call
+							<a class="btn btn-primary btn-xlg" href="tel:1-800-843-5200">
+								<span class="glyphicon glyphicon-earphone"></span> Click to Call
 							</a>
 						</blockquote>
 					</li>
@@ -172,15 +154,18 @@ b) recording, photographing, transmitting, viewing or distributing intimate or s
 							Free, confidential crisis counseling.
 							Accompaniment of survivor to the hospital and through the reporting and judicial process.<br><br>
 							<address>
-							Sunnyvale Family Justice Center<br>
-							298 South Sunnyvale Drive<br>
-							Sunnyvale, CA 94086<br><br>
-							<!-- <span class="glyphicon glyphicon-earphone"></span> 1-800-572-2782 <small>(Immediate crisis counseling assistance)</small><br>
-							<span class="glyphicon glyphicon-earphone"></span> (408) 749-0793 -->
-							<a class="btn btn-warning btn-xlg" href="1-800-572-2782"><span class="glyphicon glyphicon-earphone"></span> Click to Call
-							</a><small>(For immediate crisis counseling assistance)</small><br>
-							<a class="btn btn-primary btn-xlg" href="(408) 749-0793"><span class="glyphicon glyphicon-earphone"></span> Click to Call
-							</a>
+								Sunnyvale Family Justice Center<br>
+								298 South Sunnyvale Drive<br>
+								Sunnyvale, CA 94086<br><br>
+								<!-- <span class="glyphicon glyphicon-earphone"></span> 1-800-572-2782 <small>(Immediate crisis counseling assistance)</small><br>
+								<span class="glyphicon glyphicon-earphone"></span> (408) 749-0793 -->
+								<a class="btn btn-warning btn-xlg" href="tel:1-800-572-2782">
+									<span class="glyphicon glyphicon-earphone"></span> Click to Call
+								</a><small>(For immediate crisis counseling assistance)</small><br>
+								
+								<a class="btn btn-primary btn-xlg" href="tel:408-749-0793">
+									<span class="glyphicon glyphicon-earphone"></span> Click to Call
+								</a>
 							</address>
 						</blockquote>
 					</li>
