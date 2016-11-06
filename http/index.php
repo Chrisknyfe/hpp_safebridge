53f436b9efd461757711fc11650accacc2ac7fbf
Chrisknyfe <chrisknyfe@gmail.com>
2016-11-05 20:42:04 -0700

    Index with bootstrap
    index page now has a jumbotron and some sample headers.

--- http/index.php
+++ http/index.php
@@ -1,7 +1,33 @@
 <head>
 <title>SafeBridge - Home</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
+<div class="container">
+	<div class="jumbotron">
 	<h1>SafeBridge</h1>
 	<p>A web app to connect survivors of sexual assault to helpful resources and to help them report their experiences in a safe way. For the Stanford Health++ Hackathon. <a href="http://healthplusplus.stanford.edu/">href=http://healthplusplus.stanford.edu/</a></p>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+			  <h2>Screening &amp; Recommended Resources</h2>
+	          <p>When you're ready to talk, we provide a quick online form to help you identify what happened and provide you specific resources that will help you personally.</p>
+	          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+			</div>
+			<div class="col-md-4">
+			  <h2>Free-form Chat</h2>
+	          <p>Speak to our chat bot in perfect confidence and get answers to the questions that matter to you. When you're ready to share your experiences with a person you can upload your story to a local advocate.</p>
+	          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+			</div>
+			<div class="col-md-4">
+			  <h2>Heading</h2>
+	          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+	          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+			</div>
+		</div>
+	</div>
+</div>
+
 </body>
