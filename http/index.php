bbe34642ebbec0dd6a01fcdbc26a0d55c526f0bb
bstchan <bstchan@rocketmail.com>
2016-11-06 12:59:21 -0800

    add content and update UI for homepage

--- http/index.php
+++ http/index.php
@@ -4,16 +4,62 @@
 <title>SafeBridge - Home</title>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="modules/styling.css">
 </head>
 <body>
 <?php include "modules/navbar.php" ?>
 <div class="container">
 	<div class="jumbotron">
 	<h1>SafeBridge</h1>
-	<p>A web app to connect survivors of sexual assault to helpful resources and to help them report their experiences in a safe way. For the Stanford Health++ Hackathon. <a href="http://healthplusplus.stanford.edu/">href=http://healthplusplus.stanford.edu/</a></p>
+	<p>A web app to connect survivors of sexual assault to helpful resources and to help them report their experiences in a safe way. For the <a href="http://healthplusplus.stanford.edu/">Stanford Health++ Hackathon</a>.</p>
 	</div>
 	<div class="container">
-		<div class="row">
+		<div class="row row-content">
+			<div class="col-xs-12">
+				<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingStudent">
+							<h3 class="panel-title">
+								<a role="button" data-toggle="collapse" data-parent="#accordion" href="#student" aria-expanded="true" aria-controls="student">For Students</a>
+							</h3>
+						</div>
+						<div role="tabpanel" class="panel-collapse collapse in" id="student" aria-labelledby="headingStudent">
+							<div class="panel-body">
+								<strong>Validate your experiences to begin your path to recovery.</strong>
+								<p>If you have, or think you have, experienced an act of sexual violence, use our quick and easy self-assessment tool to dispel doubt, obtain closure, and gain the confidence you need to begin healing.</p>
+								
+								<strong>Meet Stella, your trustworthy companion.</strong>
+								<p>We built Stella from the ground up to understand your needs and cultural background, and provide instant feedback to any of your concerns.  What you choose to share with Stella is 100% confidential.  Stella is here to help you tell your story and can help you understand, process, and feel validated in everything you’re going through.</p>
+								
+								<strong>Understand your next steps, and build a plan to move forward.</strong>
+								<p>Based on your responses and information you choose to share with Stella, you will receive information about your current psychological state, the definition of your situation as defined by your school, view comparative statistics to see your progress, and recommendations for next steps to build a plan.</p>
+							</div>
+						</div>
+					</div>
+					
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingUniversity">
+							<h3 class="panel-title">
+								<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#university" aria-expanded="false" aria-controls="university">For Universities</a>
+							</h3>
+						</div>
+						<div role="tabpanel" class="panel-collapse collapse" id="university" aria-labelledby="headingUniversity">
+							<div class="panel-body">
+								<strong>Build safe learning environments and residential communities with data-driven campus reporting.</strong>
+								<p>SafeBridge generates automated reports based on anonymous user data to inform the school of campus climate, safety, and student community health.  This data can provide quantitative data on rates and classification of incidents, circumstances of incidents, and utilization of on-campus resources.</p>
+								
+								<strong>Facilitate utilization of on-campus resources and assess their efficacy.</strong>
+								<p>We connect students to the most appropriate campus resources for their situation, and reduce the barriers to accessing them.   We help the students set and adhere to a plan of action, and follow up with them to understand which programs were most useful to them.</p>
+								
+								<strong>Demonstrate your efforts towards Title IX compliance.</strong>
+								<p>Use SafeBridge campus climate reports to set targets for growth, and help avoid penalties for Title IX violations.   Explore patterns and trends in occurrences of sexual violence, and receive feedback on training program value.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row row-content">
 			<div class="col-md-4">
 			  <h2>Screening &amp; Recommended Resources</h2>
 	          <p>When you're ready to talk, we provide a quick online form to help you identify what happened and provide you specific resources that will help you personally.</p>
@@ -32,6 +78,9 @@
 		</div>
 	</div>
 </div>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="modules/bootstrap.min.js"></script>
 
 </body>
 </html>
